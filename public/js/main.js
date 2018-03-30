e7fc6f094b1e165183e6747c98f8540203c3ac81
Ben Spier <benspier@gmail.com>
2018-03-30 16:48:32 -0400

    add color toggle for button one

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 (function () {
 
   // your js goes here
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", function(element) {
+    element.target.classList.toggle("button-danger")
+  });
 
 })();
