324e4fea9ed5b315be97633726b610f551be74fc
Ben Spier <benspier@gmail.com>
2018-03-30 18:04:40 -0400

    bind all events to single event-listener

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,30 @@
   // your js goes here
   const button = document.querySelectorAll("button");
 
-  button[0].addEventListener("click", function(element) {
-    element.target.classList.toggle("button-danger")
-  });
-
-  button[1].addEventListener("click", function(element) {
-    element.target.innerText="you clicked me";
-  });
-
-  button[2].addEventListener("click", function(element) {
-    button[1].innerText="button two";
-  });
+  for (let i=0; i < button.length; i++) {
+      button[i].addEventListener("click", function(event) {
+        if (i == 0) {
+          event.target.classList.toggle("button-danger");
+        }
+        else if (i == 1) {
+          event.target.innerText="you clicked me";
+        }
+        else if (i == 2) {
+          button[1].innerText="button two";
+        }
+      });
+    };
 
 })();
+
+  // button[0].addEventListener("click", function(event) {
+  //   event.target.classList.toggle("button-danger")
+  // });
+  //
+  // button[1].addEventListener("click", function(event) {
+  //   event.target.innerText="you clicked me";
+  // });
+  //
+  // button[2].addEventListener("click", function() {
+  //   button[1].innerText="button two";
+  // });
